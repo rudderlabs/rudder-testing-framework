9f7686e4a067dd415bafa47112a4098a92347a81
Pallab Maiti <pallab@rudderstack.com>
2022-06-30 12:14:34 +0530

    travis updated & test case added

---
+++ RudderTestingFrameworkTests/RudderTestingFrameworkTests2.swift
@@ -0,0 +1,32 @@
+//
+//  RudderTestingFrameworkTests2.swift
+//  RudderTestingFrameworkTests
+//
+//  Created by Pallab Maiti on 30/06/22.
+//
+
+import XCTest
+@testable import RudderTestingFramework
+
+class RudderTestingFrameworkTests2: XCTestCase {
+
+    let rsClass = RSTestingClass()
+    
+    func testEx() {
+        let count = rsClass.countString("Pallab")
+        
+        XCTAssert(count == 6)
+    }
+    
+    func testEx2() {
+        let count = rsClass.countString("Cat")
+        
+        XCTAssert(count == 3)
+    }
+    
+    func testEx3() {
+        let count = rsClass.countString("Batman")
+        
+        XCTAssertFalse(count == 3)
+    }
+}
